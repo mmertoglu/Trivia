79f98b28496dcfb7c4a4c56b640fa617edc430e4
Muhammed Mertoğlu <97661173+mmertoglu@users.noreply.github.com>
2022-07-04 15:28:29 +0300

    questions screen added

--- Trivia/App.js
+++ Trivia/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {NavigationContainer} from '@react-navigation/native'
 import {createNativeStackNavigator} from '@react-navigation/native-stack'
 import Welcome from './src/screens/Welcome/Welcome';
+import Questions from './src/screens/Questions/Questions';
 
 const Stack = createNativeStackNavigator();
 
@@ -10,6 +11,7 @@ const App = () => {
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown:false}} >
         <Stack.Screen name='WelcomeScreen' component={Welcome} />
+        <Stack.Screen name='QuestionsScreen' component={Questions} />
       </Stack.Navigator>
     </NavigationContainer>
   )
---
+++ Trivia/src/screens/Questions/Questions.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {View,Text} from 'react-native'
+import styles from './Questions.style'
+const Questions = () => {
+    return(
+        <View>
+            <Text>Questions</Text>
+        </View>
+    )
+}
+
+export default Questions;
