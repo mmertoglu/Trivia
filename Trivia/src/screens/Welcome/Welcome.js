114e338c8e4de9a64ba3e748dd59581529a8bbc3
Muhammed Mertoğlu <97661173+mmertoglu@users.noreply.github.com>
2022-07-04 15:28:38 +0300

    style

--- Trivia/src/screens/Welcome/Welcome.js
+++ Trivia/src/screens/Welcome/Welcome.js
@@ -1,12 +1,18 @@
 import React from 'react'
-import {View,Text,StatusBar} from 'react-native'
+import {View,Text,StatusBar, TouchableOpacity} from 'react-native'
 import styles from './Welcome.style'
 import LinearGradient from 'react-native-linear-gradient'
-const Welcome = () => {
+const Welcome = ({navigation}) => {
+    const goQuestions = () => {
+        navigation.navigate('QuestionsScreen')
+    }
     return(
         <LinearGradient colors={['#000428','#004e92']}  style={styles.container} >
             <StatusBar translucent={true} backgroundColor={'transparent'} />
-            <Text style={styles.text} >Welcome to Trivia</Text>
+            <Text style={styles.text} >Trivia'ya Hoşgeldin</Text>
+            <TouchableOpacity onPress={goQuestions} style={styles.basla_buton} >
+                <Text style={styles.basla_text} >Oyuna Başla</Text>
+            </TouchableOpacity>
         </LinearGradient>
     )
 }
--- Trivia/src/screens/Welcome/Welcome.style.js
+++ Trivia/src/screens/Welcome/Welcome.style.js
@@ -9,5 +9,19 @@ export default StyleSheet.create({
         color:'white',
         fontFamily:'Righteous-Regular',
         fontSize:30
+    },
+    basla_buton : {
+        backgroundColor:'white',
+        padding:10,
+        marginTop:50,
+        borderRadius:8,
+        elevation:8,
+        width:200,
+        alignItems:'center'
+    },
+    basla_text:{
+        fontSize:16,
+        fontWeight:'bold',
+        color:'black'
     }
 })
